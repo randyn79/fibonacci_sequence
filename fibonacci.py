16cd05a1ccfc659ac0364761afdc1fb9623eaa48
Randy Noles <randynoles@gmail.com>
2021-12-01 17:58:53 -0500

    Initial commit.

---
+++ fibonacci.py
@@ -0,0 +1,33 @@
+sequence = []
+
+def fibonacci(maximum, first = 0, second = 1):
+    if second <= maximum:
+        sequence.append(first)
+        sequence.append(second)
+        temp = second
+        first = first + second
+        second = temp + first
+
+
+        fibonacci(maximum, first, second)
+    
+    return sequence
+
+if __name__ == "__main__":
+    print('Fibonacci Sequence')
+    print('=' * 18)
+    print()
+    print('What is the maximum number in the sequence you would like to return?')
+    print('Be careful in entering a number too large or the system may error.')
+    maximum = int(input('Maximum value:  '))
+
+    if maximum >= 0 and isinstance(maximum, int):
+        sequence = fibonacci(maximum)
+        print(sequence)
+    else:
+        print('Invalid input, maximum must be a positive integer.')
+    
+
+    
+    
+
